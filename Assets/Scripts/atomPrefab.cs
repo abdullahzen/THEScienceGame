6f9b31a6e7352f99350e85edbec474e52a5d67e9
abdullahzen <abdullah.hzen@gmail.com>
2023-02-05 22:20:26 -0500

    [#33]: added more small details to molecule game (#45)
    * THEScienceGame#15: implemented core of minigame
    
    * [#33]: added in circle around selected atoms and updated the submit script
    
    * [#33]: added another molecule question
    
    for some reason i had trouble with making the atoms float so i let it go
    
    ---------
    
    Co-authored-by: sarahprimavera <sarahprimavera787@gmail.com>

---
+++ Assets/Scripts/atomPrefab.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class atomPrefab : MonoBehaviour
+{
+    [SerializeField]
+    private Button atomButton;
+    [SerializeField]
+    private submitButton submit;
+    private Glow glow;
+    private int clickCount;
+    void Start()
+    {
+        Button btn = atomButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        glow = atomButton.GetComponentInChildren<Glow>();
+        glow.glowOff();
+        clickCount = -1;
+    }
+
+    void TaskOnClick()
+    {
+        clickCount++;
+        if (clickCount%2 == 0)
+        {
+            gameObject.tag = "Clicked";
+            submit.addOrRemoveAtom(atomButton.name, false);
+            glow.glowOn();
+        }
+        else
+        {
+            gameObject.tag = "Button";
+            submit.addOrRemoveAtom(atomButton.name, true);
+            glow.glowOff();
+        }
+        
+    }
+
+    void glowOffPrefab()
+    {
+        glow.glowOff();
+    }
+}
